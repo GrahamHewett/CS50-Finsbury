0b6d6cdb9fc49db1aebd5b9150da3382605d6a75
Will <wfoster12@gmail.com>
2018-07-16 23:42:04 +0100

    Completed Mario less solution

---
+++ pset1/mario/mario.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int pyramid(int h);
+
+int main(void)
+{
+    int input = get_int("Please enter your pyramid height: ");
+
+    if (input >= 0 && input < 24) {
+
+    for (int l = 0; l < input; l++){
+        //To print the spaces(s)
+        for(int s = input - l; s > 1; s--){
+            printf(" ");
+        }
+        //To print the bricks(b)
+        for (int b = 0; b< l+2; b++){
+            printf("#");
+        }
+        //New line
+            printf("\n");
+        }
+
+    } else {
+        main();
+    }
+
+}
